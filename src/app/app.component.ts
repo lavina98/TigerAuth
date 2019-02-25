6715347f08469dc23481f739f4f02f18eba234d5
Gayatri246 <rmnipankar@gmail.com>
2019-02-25 19:35:06 +0530

    navbar removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+  usernameValid: boolean;
+  constructor(private userService: UserService) {
+
+  }
+  ngOnInit() {
+    if(this.userService.getUsername() === undefined) {
+      this.usernameValid = false;
+    } else {
+      this.usernameValid = true;
+    }
+  }
 }
