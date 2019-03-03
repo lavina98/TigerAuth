67cc5ba1ffd590410fe26c387718abb35b58d9fd
mihir <mihirnd@gmail.com>
2019-03-03 10:30:54 +0530

    login component changed

--- src/app/shared/backend-ip.ts
+++ src/app/shared/backend-ip.ts
@@ -1,10 +1,10 @@
 // Siddharth
 
-// export const ip = 'https://172.30.1.138:3000';
+export const ip = 'https://172.30.1.138:3000';
 
 // Varsha
 
-export const ip = 'https://172.30.7.229:3000';
+// export const ip = 'https://172.30.7.229:3000';
 
 // Varsha on lg
 
