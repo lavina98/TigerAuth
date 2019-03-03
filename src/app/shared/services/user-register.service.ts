420f3a9f0cba8b27e78e01037a73ed10c35996b4
shruti-97 <belgali.shruti@gmail.com>
2019-03-03 05:38:32 +0530

    sanity check commit 2

--- src/app/shared/services/user-register.service.ts
+++ src/app/shared/services/user-register.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { ip } from '../backend-ip';
 import { IUserDetails } from '../models/user-details.model';
 
