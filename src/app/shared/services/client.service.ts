b25909a52acea1c57fb3c68e210a8d9d862b3e12
mihir <mihirnd@gmail.com>
2019-03-03 07:23:17 +0530

    tiger auth login

--- src/app/shared/services/client.service.ts
+++ src/app/shared/services/client.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IClientDetails } from '../models/client.model';
+import { IClientDetails, IClient } from '../models/client.model';
 import { HttpClient } from '@angular/common/http';
 import { ip } from '../backend-ip';
 
